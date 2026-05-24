1281058ef85338b48f8b19ae7eb7bf7c3a259f75
Emanuele D'Andrea <emanueledandrea@proton.me>
2026-05-24 14:46:05 +0200

    general debloat and improvements

--- build.sh
+++ build.sh
@@ -3,23 +3,14 @@
 set -e
 
 deploy() {
+  # rm -r target
   cp -Rf ./src ./target
-  sed -i '/^@import/d' ./target/main.css
-  cd ./target/styles
-  cat header.css blog.css reset.css footer.css ../main.css > ../output
-  mv about.css ../
-  mv post.css ../
-  cd ../
-  rm ./main.css && rm -r ./styles
-  mv ./output ./main.css
-  cd ../
   # https://github.com/tdewolff/minify
   minify --recursive --inplace target
   rsync --archive --delete --info=progress target/ server:/var/www/emanueledandrea.eu
 }
 
 case "$1" in
-# convert) convert ;;
 deploy) deploy ;;
 serve)
   cd src
